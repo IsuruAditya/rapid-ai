260f4e4b30d8115f4816beca4a23722043185e05
Isuru Sandaruwan <isuruadithyasandaruwan@gmail.com>
2025-07-28 21:04:02 +0530

    Implement code changes to enhance functionality and improve performance

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -1,4 +1,5 @@
 import logo from "./logo.svg";
+import logo2 from "./logo2.svg";
 import gradientBackground from "./gradientBackground.png";
 import user_group from "./user_group.png";
 import star_icon from "./star_icon.svg";
@@ -12,6 +13,7 @@ import ai_gen_img_3 from "./ai_gen_img_3.png";
 
 export const assets = {
     logo,
+    logo2,
     gradientBackground,
     user_group,
     star_icon,
