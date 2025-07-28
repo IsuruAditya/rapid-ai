4a3f5a3cf04162ddb5cf4f22c921baa2e0f5dd48
Isuru Sandaruwan <isuruadithyasandaruwan@gmail.com>
2025-07-28 21:03:22 +0530

    feat: implement fetching and liking functionality in Community component with loading state and error handling

--- client/src/pages/Community.jsx
+++ client/src/pages/Community.jsx
@@ -1,21 +1,83 @@
 import React, { useEffect, useState } from "react";
 import { useUser } from "@clerk/clerk-react";
-import { dummyPublishedCreationData } from "../assets/assets";
 import { Heart } from "lucide-react";
+import axios from "axios";
+import { useAuth } from "@clerk/clerk-react";
+import toast from "react-hot-toast";
 
+axios.defaults.baseURL = import.meta.env.VITE_BASE_URL;
 const Community = () => {
   const [creations, setCreations] = useState([]);
   const { user } = useUser();
+  const [loading, setLoading] = useState(true);
+
+  const { getToken } = useAuth();
 
   const fetchCreations = async () => {
-    setCreations(dummyPublishedCreationData);
+    try {
+      const { data } = await axios.get("/api/user/get-published-creations", {
+        headers: {
+          Authorization: `Bearer ${await getToken()}`,
+        },
+      });
+
+      if (data.success) {
+        setCreations(data.creations);
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      console.error(error.message);
+      toast.error(error.message);
+    }
+
+    setLoading(false);
+  };
+
+  const imageLikeToggle = async (id) => {
+    try {
+      /*setCreations(prevCreations =>
+      prevCreations.map(creation => {
+        if (creation.id === id) {
+          const isLiked = creation.likes.includes(user.id);
+          return {
+            ...creation,
+            likes: isLiked
+              ? creation.likes.filter(user => user !== user.id)
+              : [...creation.likes, user.id],
+          };
+        }
+        return creation;
+      })
+    );*/
+
+      const { data } = await axios.post(
+        "/api/user/toggle-like-creation",
+        { id },
+        {
+          headers: {
+            Authorization: `Bearer ${await getToken()}`,
+          },
+        }
+      );
+
+      if (data.success) {
+        toast.success(data.message);
+        await fetchCreations();
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      console.error(error.message);
+      toast.error(error.message);
+    }
   };
 
   useEffect(() => {
     if (user) fetchCreations();
   }, [user]);
 
-  return (
+  return !loading ? (
     <div className="flex-1 h-full flex flex-col gap-4 p-6">
       Creations
       <div className="bg-white h-full w-full rounded-xl overflow-y-scroll">
@@ -36,6 +98,7 @@ const Community = () => {
               <div className="flex gap-1 items-center">
                 <p>{creation.likes.length}</p>
                 <Heart
+                  onClick={() => imageLikeToggle(creation.id)}
                   className={`min-w-5 h-5 hover:scale-110 cursor-pointer
                    ${
                      creation.likes.includes(user.id)
@@ -48,6 +111,10 @@ const Community = () => {
           </div>
         ))}
       </div>
+    </div>
+  ) : (
+    <div className="flex justify-center items-center h-full">
+      <span className="w-10 h-10 my-1 rounded-full border-3 border-primary border-t-transparent animate-spin"></span>
     </div>
   );
 };
